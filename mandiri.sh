d081d43a663a203aa1c0cc689f8eddeac7289160
Elbert Jonathan <m26415048@john.petra.ac.id>
2016-09-15 09:56:53 +0700

    Mandiri

---
+++ mandiri.sh
@@ -0,0 +1,4 @@
+beli=`curl -s http://klikbca.com | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f2`
+jual=`curl -s http://klikbca.com | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f3`
+echo "Harga Jual:"$jual
+echo "Harga Beli:"$beli

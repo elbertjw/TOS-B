000b9f58f88d2359968122b2f14863840ac6cf59
Elbert Jonathan <m26415048@john.petra.ac.id>
2016-09-21 15:07:59 +0700

    PR_Crontab

---
+++ pr_crontab.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+0 11,18 * * 1-5 /home/TOS-B/emas.txt
+0 8-18 * * 1-5 /home/TOS-B/bcaSGD.txt
+0 8-18 * * 1-5 /home/TOS-B/bcaUSD.txt
+0 8-18 * * 1-5 /home/TOS-B/mandiriSGD.txt
+0 8-18 * * 1-5 /home/TOS-B/mandiriUSD.txt
+
